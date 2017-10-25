5d7bd42abf62510a5da9ea0b384308adae8d8203
sadiqultra <sadikultra@gmail.com>
2017-10-25 10:45:02 +0600

    helpers:Notifications

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,8 @@
 import { AsyncStorage } from 'react-native'
+import { Notifications, Permissions } from 'expo'
 
 export const FLASHCARDS_STORAGE_KEY = 'Flashcards:data:dev:1'
+const FLASHCARDS_NOTIFICATION_KEY = 'Flashcards:notifications'
 
 export function  generateId(Id = '') {
   Id = Id.replace(/\W/g, '_')
@@ -43,4 +45,55 @@ export function addCardToDeck (card, deck, id) {
   AsyncStorage.mergeItem(FLASHCARDS_STORAGE_KEY, JSON.stringify({
     [id]: deck,
   }))
+}
+
+export function clearLocalNotification () {
+  return AsyncStorage.removeItem(FLASHCARDS_NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationsAsync)
+}
+
+function createNotification () {
+  return {
+    title: '👋 Log your stats!',
+    body:  '👋 don\'t forget to log your stats for today',
+    ios: {
+      sound: true,
+    },
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true,
+    }
+  }
+}
+
+export function setLocalNotification () {
+  AsyncStorage.getItem(FLASHCARDS_NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if(data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync()
+
+              let tomorrow = new Date()
+              tomorrow.setDate(tomorrow.getDate() + 1)
+              tomorrow.setHours(20)
+              tomorrow.setMinutes(0)
+
+              Notifications.scheduleLocalNotificationAsync(
+               createNotification(),
+               {
+                 time: tomorrow,
+                 repeat: 'day',
+               }
+              )
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+            }
+          })
+      }
+    })
 }
